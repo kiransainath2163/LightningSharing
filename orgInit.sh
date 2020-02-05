143c107cd456c03c4fb2841cf7bdedb26f2156e2
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-05 08:03:21 -0600

    v48 no communities

--- orgInit.sh
+++ orgInit.sh
@@ -5,11 +5,11 @@ sfdx shane:github:src:install -g mshanemc -r community-boilerplate -p force-app
 sfdx force:user:password:generate
 
 sfdx force:source:push -f
-sfdx shane:communities:publish
+# sfdx shane:communities:publish
 sfdx force:user:permset:assign -n TestingPerms
 
-sfdx force:apex:execute -f scripts/roleAssign.cls 
-sfdx force:apex:execute -f scripts/communityUserCreate.cls 
+# sfdx force:apex:execute -f scripts/roleAssign.cls 
+# sfdx force:apex:execute -f scripts/communityUserCreate.cls 
 
 # create some records
 sfdx force:data:tree:import -f data/PrivateTestObject__c.json
@@ -18,4 +18,4 @@ sfdx force:data:tree:import -f data/ReadOnlyTestObject__c.json
 # for security testing
 sfdx force:user:create generatepassword=true FirstName=Test LastName=Privilege permsets=TestingPerms profileName="Standard User"
 
-sfdx force:org:open
+sfdx force:data:tree:import -f data/ReadOnlyTestObject__c.json
